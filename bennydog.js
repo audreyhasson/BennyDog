120e08417b6a747207d8081425226114e7610679
auds <amhasson4@gmail.com>
2020-03-23 00:11:40 -0400

    added carousel + pix

--- bennydog.js
+++ bennydog.js
@@ -2,6 +2,17 @@
 
 console.log('hello');
 
-$(document).ready(function($){
-  $(".owl-carousel").owlCarousel();
+jQuery(function($){
+  $(document).ready(function() {
+setTimeout(function(){$(".owl-carousel").owlCarousel()},2000);
+})
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items: 1,
+    autoplay: true,
+    autoplayHoverPause: true,
+})
